1107b52fa7004b4441e3956ca0bfd0d309d14f70
raj_kayasth <rajakayasth19@gmail.com>
2022-07-20 18:42:46 +0530

    chip click listener and filter

--- app/src/main/java/com/example/nav_recycler_task/fragment/Tab3Fragment.kt
+++ app/src/main/java/com/example/nav_recycler_task/fragment/Tab3Fragment.kt
@@ -1,12 +1,17 @@
 package com.example.nav_recycler_task.fragment
 
+import android.content.res.ColorStateList
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.children
+import androidx.core.view.forEach
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nav_recycler_task.R
@@ -18,6 +23,7 @@ import com.example.nav_recycler_task.respository.NewsRespository
 import com.example.nav_recycler_task.viewModels.DemoViewModel
 import com.example.nav_recycler_task.viewModels.DemoViewModelFactory
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.google.android.material.textview.MaterialTextView
 
@@ -28,6 +34,7 @@ class Tab3Fragment : Fragment() {
     private val binding get() = _binding!!
     lateinit var recyclerAdopter: RecyclerAdopter
     lateinit var newList: ArrayList<Article>
+    lateinit var authorList: ArrayList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,8 +55,9 @@ class Tab3Fragment : Fragment() {
 
         newList = arrayListOf()
         viewModel.quotes.observe(viewLifecycleOwner) {
-          //  Log.e("Tab3", it.articles.toString())
+            //  Log.e("Tab3", it.articles.toString())
             binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
             newList.addAll(it.articles)
             recyclerAdopter = RecyclerAdopter(it.articles)
             binding.recyclerView.adapter = recyclerAdopter
@@ -63,19 +71,46 @@ class Tab3Fragment : Fragment() {
 
             val btnClose = view.findViewById<MaterialTextView>(R.id.txtViewReset)
             val btnDone = view.findViewById<MaterialTextView>(R.id.txtViewDone)
-            val editText = view.findViewById<EditText>(R.id.edTxtAuthor)
+          //  val editText = view.findViewById<EditText>(R.id.edTxtAuthor)
 
             val chipGroup = view.findViewById<ChipGroup>(R.id.chipGroup)
+            authorList = arrayListOf()
 
 
-            btnDone.setOnClickListener {
-                author = editText.text.toString()
 
-               val list = newList.filter { it.author == author }
-               // Log.e("list", chip)
-                binding.recyclerView.adapter = RecyclerAdopter(list)
-                dialog.dismiss()
+            newList.filter {
+                authorList.addAll(listOf(it.author))
             }
+            Log.e("authorList", authorList.toString())
+
+
+
+
+            for (genre in 0 until authorList.size) {
+                val chip = Chip(requireContext())
+                chip.text = authorList[genre]
+                chip.id = R.id.filerChip_ID
+                chipGroup.addView(chip)
+                chip.isCheckable = true
+            }
+
+            chipGroup.forEach { child ->
+                (child as Chip).setOnClickListener {
+                   child.isChecked = true
+                    Log.e("text",child.text.toString())
+                    for (i in 0 until authorList.size) {
+                        btnDone.setOnClickListener {
+                            val list = newList.filter {
+                               it.author == child.text.toString()
+                            }
+                            Log.e("list", authorList.toString())
+                            binding.recyclerView.adapter = RecyclerAdopter(list)
+                            dialog.dismiss()
+                        }
+                    }
+                }
+            }
+
 
 
             btnClose.setOnClickListener {
